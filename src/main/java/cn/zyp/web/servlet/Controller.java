c7c31110f9d9f401b0300543caf0ea68ec8709b8
zhengyp <1752649897@qq.com>
2018-05-22 08:30:05 +0800

    springmvc-1.0  轻量级MVC框架

--- src/main/java/cn/zyp/web/servlet/Controller.java
+++ src/main/java/cn/zyp/web/servlet/Controller.java
@@ -3,6 +3,9 @@ package cn.zyp.web.servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 
+ */
 public interface Controller {
 
     ModelAndView handler(HttpServletRequest request, HttpServletResponse response);
